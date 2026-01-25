1a7de2b0c50c1844dc1095c6fd17ff3253743e88
Prayash Thapa <prayashthapa15@gmail.com>
2026-01-25 11:49:25 +0545

    refactor:: buttons spawn logic

--- src/menu.rs
+++ src/menu.rs
@@ -6,20 +6,28 @@ pub struct MenuPlugin;
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(AppState::MainMenu), setup_menu)
-           .add_systems(Update, menu_action.run_if(in_state(AppState::MainMenu)))
-           .add_systems(OnExit(AppState::MainMenu), cleanup_menu);
+            .add_systems(Update, menu_action.run_if(in_state(AppState::MainMenu)))
+            .add_systems(OnExit(AppState::MainMenu), cleanup_menu);
     }
 }
 
 #[derive(Component)]
 struct MenuData;
 
-#[derive(Component)]
-struct PlayButton;
+#[derive(Component, Clone, Copy)]
+enum MenuButtonAction {
+    Play,
+    Quit,
+}
 
 fn setup_menu(mut commands: Commands) {
     // Camera
     commands.spawn((Camera2d::default(), MenuData));
+
+    let buttons = [
+        ("Play", MenuButtonAction::Play),
+        ("Quit", MenuButtonAction::Quit),
+    ];
 
     // UI
     commands
@@ -27,53 +35,63 @@ fn setup_menu(mut commands: Commands) {
             Node {
                 width: Val::Percent(100.0),
                 height: Val::Percent(100.0),
+                flex_direction: FlexDirection::Column,
                 align_items: AlignItems::Center,
                 justify_content: JustifyContent::Center,
+                row_gap: Val::Px(10.0),
                 ..default()
             },
             MenuData,
         ))
         .with_children(|parent| {
-            parent
-                .spawn((
-                    Button,
-                    Node {
-                        width: Val::Px(150.0),
-                        height: Val::Px(65.0),
-                        border: UiRect::all(Val::Px(5.0)),
-                        justify_content: JustifyContent::Center,
-                        align_items: AlignItems::Center,
-                        ..default()
-                    },
-                    BackgroundColor::from(Color::srgb(0.15, 0.15, 0.15)),
-                    BorderColor::from(Color::WHITE),
-                    PlayButton,
-                ))
-                .with_children(|parent| {
-                    parent.spawn((
-                        Text::new("Play"),
-                        TextFont {
-                            font_size: 40.0,
+            for (text, action) in buttons {
+                parent
+                    .spawn((
+                        Button,
+                        Node {
+                            width: Val::Px(150.0),
+                            height: Val::Px(65.0),
+                            border: UiRect::all(Val::Px(5.0)),
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
                             ..default()
                         },
-                        TextColor(Color::WHITE),
-                    ));
-                });
+                        BackgroundColor::from(Color::srgb(0.15, 0.15, 0.15)),
+                        BorderColor::from(Color::WHITE),
+                        action,
+                    ))
+                    .with_children(|parent| {
+                        parent.spawn((
+                            Text::new(text),
+                            TextFont {
+                                font_size: 40.0,
+                                ..default()
+                            },
+                            TextColor(Color::WHITE),
+                        ));
+                    });
+            }
         });
 }
 
 fn menu_action(
     interaction_query: Query<
-        (&Interaction, &BackgroundColor),
-        (Changed<Interaction>, With<Button>, With<PlayButton>),
+        (&Interaction, &MenuButtonAction),
+        (Changed<Interaction>, With<Button>),
     >,
     mut next_state: ResMut<NextState<AppState>>,
 ) {
-    for (interaction, _color) in &interaction_query {
+    for (interaction, action) in &interaction_query {
         match *interaction {
-            Interaction::Pressed => {
-                next_state.set(AppState::InGame);
-            }
+            Interaction::Pressed => match action {
+                MenuButtonAction::Play => {
+                    next_state.set(AppState::InGame);
+                }
+                MenuButtonAction::Quit => {
+                    // TODO: Implement proper AppExit when possible
+                    println!("Quit pressed");
+                }
+            },
             _ => {}
         }
     }
